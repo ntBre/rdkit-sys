a9d8402335d7b45802dc05a13283b2a40cd1b0b6
Brent Westbrook <brentrwestbrook@gmail.com>
2024-01-11 21:14:02 -0500

    replicate try.c in Rust

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
 mod tests {
-    use std::ffi::CString;
+    use std::ffi::{CStr, CString};
 
     use super::*;
 
@@ -15,7 +15,15 @@ mod tests {
         let path = "/home/brent/omsf/chembl/chembl_33.sdf";
         let cpath = CString::new(path).unwrap();
         unsafe {
-            RDKit_create_mol_supplier(cpath.as_ptr());
+            let m = RDKit_create_mol_supplier(cpath.as_ptr());
+            let mut mol = ROMol_new();
+            while !RDKit_mol_supplier_at_end(m) {
+                ROMol_delete(mol);
+                mol = RDKit_mol_supplier_next(m);
+                let smiles = RDKit_MolToSmiles(mol);
+                let s = CStr::from_ptr(smiles);
+                println!("{}", s.to_str().unwrap());
+            }
         }
     }
 }
