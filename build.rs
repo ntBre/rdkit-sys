a865565a6a8f692c5a75355b5cf1df5d9d1c604b
Brent Westbrook <brentrwestbrook@gmail.com>
2024-01-19 15:35:19 -0500

    simplify build script

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
-    let rdkit_root = std::env::var("RDROOT")
+    let rdroot = std::env::var("RDROOT")
         .unwrap_or_else(|_| "/home/brent/omsf/clone/rdkit".to_owned());
 
     Command::new("make")
@@ -11,28 +11,19 @@ fn main() {
         .output()
         .unwrap();
 
-    // Tell cargo to look for shared libraries in the specified directory
-    println!("cargo:rustc-link-search={rdkit_root}/build/lib");
-
     let include = std::fs::canonicalize("./include").unwrap();
     let include = include.display();
     println!("cargo:rustc-link-search={include}");
 
-    println!("cargo:rustc-link-lib=RDKitGraphMol");
-    println!("cargo:rustc-link-lib=RDKitSmilesParse");
-    println!("cargo:rustc-link-lib=RDKitFileParsers");
     println!("cargo:rustc-link-lib=shim");
 
-    println!(
-        "cargo:rustc-env=LD_LIBRARY_PATH={include}:{rdkit_root}/build/lib"
-    );
+    println!("cargo:rustc-env=LD_LIBRARY_PATH={include}:{rdroot}/build/lib");
 
     println!("cargo:rerun-if-changed=wrapper.h");
     println!("cargo:rerun-if-env-changed=RDROOT");
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_arg(format!("-I{rdkit_root}/Code"))
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
