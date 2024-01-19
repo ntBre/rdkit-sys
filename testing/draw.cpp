c071218373dca1acedca44e69d5b7f5de78fe2fb
Brent Westbrook <brentrwestbrook@gmail.com>
2024-01-19 12:03:47 -0500

    add example for drawing an svg molecule

---
+++ testing/draw.cpp
@@ -0,0 +1,33 @@
+#include <GraphMol/GraphMol.h>
+#include <GraphMol/MolDraw2D/MolDraw2DSVG.h>
+#include <GraphMol/MolDraw2D/MolDraw2DUtils.h>
+#include <GraphMol/SmilesParse/SmilesParse.h>
+
+#include <fstream>
+#include <iostream>
+
+using namespace RDKit;
+using namespace std;
+
+int main() {
+  ofstream output;
+  output.open("mol.html");
+
+  string smiles = "C1=CC=CC=C1";
+
+  // args to the function
+  ROMol *mol = SmilesToMol(smiles);
+  int width = 400;
+  int height = 300;
+  string legend = smiles;
+  vector<int> highlight_atoms = {1, 2, 3, 4};
+
+  MolDraw2DSVG drawer = MolDraw2DSVG(width, height);
+  MolDraw2DUtils::prepareAndDrawMolecule(drawer, *mol, legend,
+                                         &highlight_atoms);
+  string svg = drawer.getDrawingText();
+
+  output << svg;
+  output.close();
+  return 0;
+}
