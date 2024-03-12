59300c1f23733c716e6afc5f1c32df064b8dcc4a
Brent Westbrook <brentrwestbrook@gmail.com>
2024-03-12 13:52:14 -0400

    copy probe_mol.sdf from RDKit and add real test, delete main
    Code/GraphMol/MolAlign/test_data/probe_mol.sdf

--- src/lib.rs
+++ src/lib.rs
@@ -11,10 +11,12 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
-    fn one() {
-        let path = "/home/brent/omsf/chembl/data/chembl_33.sdf";
+    fn mol_supplier() {
+        let path = "testfiles/probe_mol.sdf";
         let cpath = CString::new(path).unwrap();
+        let smarts =
+            CString::new("[#6X4:1]-[#6X4:2]-[#6X4:3]-[#6X4:4]").unwrap();
+
         unsafe {
             let m = RDKit_create_mol_supplier(cpath.as_ptr(), true);
             let mut mol = RDKit_ROMol_new();
@@ -22,9 +24,23 @@ mod tests {
                 RDKit_ROMol_delete(mol);
                 mol = RDKit_mol_supplier_next(m);
                 let smiles = RDKit_MolToSmiles(mol);
+                let mut len = 0;
+                let mut match_size = 0;
+                let matches = find_smarts_matches(
+                    mol,
+                    smarts.as_ptr(),
+                    &mut len,
+                    &mut match_size,
+                );
+                let matches = Vec::from_raw_parts(matches, len, len);
+                for (i, mat) in matches.chunks(match_size).enumerate() {
+                    println!(
+                        "Match {i} : {} {} {} {}",
+                        mat[0], mat[1], mat[2], mat[3]
+                    );
+                }
                 let s = CStr::from_ptr(smiles);
                 println!("{}", s.to_str().unwrap());
-                return;
             }
         }
     }
